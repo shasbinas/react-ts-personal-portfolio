8cc52c543b8576ac6f57156ff7544a0932b5e002
Shasbin  AS <shasbinas1@gmail.com>
2025-12-22 22:23:50 +0530

    feat:education session corrected

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -5,27 +5,27 @@ import { EducationCard } from './ui/EducationCard';
 const education = [
   {
     degree: 'Bachelor of Computer Applications (BCA)',
-    institution: 'Sreenarayanaguru Open University',
-    period: 'Jul 2025 – Expected Apr 2028',
+    institution: 'Yenepoya University',
+    period: 'oct 2025 – Expected oct 2028',
     score: 'Ongoing',
   },
-  {
-    degree: 'Full Stack Development (MERN)',
-    institution: 'Brototype',
-    period: 'Jul 2022 – Aug 2023',
-    score: '',
-  },
+  // {
+  //   degree: 'Full Stack Development (MERN)',
+  //   institution: 'Brototype',
+  //   period: 'Jul 2022 – Aug 2023',
+  //   score: '',
+  // },
   {
     degree: 'Senior Secondary (Plus Two)',
-    institution: 'National Institute of Open Schooling',
-    period: 'Jul 2014 – Mar 2015',
-    score: '80%',
+    institution: 'Lbsm Higher Secondary School',
+    period: 'Jul 2021 – Mar 2023',
+    score: '74%',
   },
   {
     degree: 'Secondary (SSLC)',
-    institution: `St. Antony's Higher Secondary School Moorkanad`,
-    period: 'Jul 2007 – Mar 2008',
-    score: '80%',
+    institution: `Lbsm Higher Secondary School`,
+    period: 'Jul 2020 – Mar 2021',
+    score: '92%',
   },
 ];
 
